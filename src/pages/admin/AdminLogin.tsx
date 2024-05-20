7a469233d0aa9f397748041a8a1ddf6e99aedef1
Mohammed Sinan Mp <sinan.m.p333@gmail.com>
2024-05-20 18:13:09 +0530

    image paths and icons paths changed

--- src/pages/admin/AdminLogin.tsx
+++ src/pages/admin/AdminLogin.tsx
@@ -5,7 +5,7 @@ function AdminLogin() {
     const changePage =''
     return (
       <div className="relative">
-        <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url("/src/assets/images/expressive-young-woman-posing-studio.jpg")' }}>
+        <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url("/public/assets/images/expressive-young-woman-posing-studio.jpg")' }}>
           <Auth pageName="Login" role="admin" changePage={changePage} signupInputs="hidden"/>
         </div>
       </div>
--- src/pages/doctor/DoctorLogin.tsx
+++ src/pages/doctor/DoctorLogin.tsx
@@ -5,7 +5,7 @@ function DoctorLogin() {
     const changePage =''
   return (
     <div className="relative">
-      <div className="bg-cover bg-center h-screen " style={{ backgroundImage: 'url("/src/assets/images/expressive-young-woman-posing-studio.jpg")' }}>
+      <div className="bg-cover bg-center h-screen " style={{ backgroundImage: 'url("/public/assets/images/expressive-young-woman-posing-studio.jpg")' }}>
         <Auth pageName="Login" role="doctor" changePage={changePage}  signupInputs="hidden"/>
       </div>
     </div>
