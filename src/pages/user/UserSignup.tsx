4469bb016f6c27e86bef9940aee5998301a3ed25
Mohammed Sinan Mp <sinan.m.p333@gmail.com>
2024-05-20 17:10:20 +0530

    Base url logged

--- src/pages/user/UserSignup.tsx
+++ src/pages/user/UserSignup.tsx
@@ -3,7 +3,7 @@ import Auth from "../../components/common/Auth"
 
 function UserSignup() {
     const changePage ='Already have an account? '
-    console.log("changePage", import.meta.env.VITE_BASE_UR);
+    console.log("changePage", import.meta.env.VITE_BASE_URL);
     
   return (
     <div className="relative">
