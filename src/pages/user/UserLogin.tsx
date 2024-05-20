2fa16d3b1ea0e3178bfdd012e402e3c751cd8d99
Mohammed Sinan Mp <sinan.m.p333@gmail.com>
2024-05-20 18:19:45 +0530

    User login imge path changed

--- src/pages/user/UserLogin.tsx
+++ src/pages/user/UserLogin.tsx
@@ -4,7 +4,7 @@ function UserLogin() {
     const changePage ='New to Mediheal? '
   return (
     <div className="relative">
-      <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url("/public/assets/images/expressive-young-woman-posing-studio.jpg")' }}>
+      <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url("/assets/images/expressive-young-woman-posing-studio.jpg")' }}>
         <Auth pageName="Login" role="user"  changePage={changePage}  signupInputs="hidden" />
       </div>
     </div>
