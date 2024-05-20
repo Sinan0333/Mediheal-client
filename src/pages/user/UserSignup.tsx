bd96a35d5450d3ff3c142c258381da72ef80c2e1
Mohammed Sinan Mp <sinan.m.p333@gmail.com>
2024-05-20 17:07:10 +0530

    Base url changed

--- src/pages/user/UserSignup.tsx
+++ src/pages/user/UserSignup.tsx
@@ -3,9 +3,11 @@ import Auth from "../../components/common/Auth"
 
 function UserSignup() {
     const changePage ='Already have an account? '
+    console.log("changePage", import.meta.env.BASE_URL);
+    
   return (
     <div className="relative">
-      <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url("src/assets/images/expressive-young-woman-posing-studio.jpg")' }}>
+      <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url("/src/assets/images/expressive-young-woman-posing-studio.jpg")' }}>
         <Auth pageName="Signup" changePage={changePage} role="user"  signupInputs="block"/>
       </div>
     </div>
